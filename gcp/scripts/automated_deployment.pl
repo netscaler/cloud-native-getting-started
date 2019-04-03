48022edb93705328af826726167c793a079a527b
Avinash Kumar <vavinash.kumar@citrix.com>
2019-04-03 23:41:28 +0530

    adm config files

--- gcp/scripts/automated_deployment.pl
+++ gcp/scripts/automated_deployment.pl
@@ -39,10 +39,6 @@ if ($operation eq "delete") {
 
 }
 
-my $image_location;
-if ($ARGV[0] ne "delete") {
-    $image_location = $ARGV[0];
-}
 my $project_id = $ENV{'GOOGLE_CLOUD_PROJECT'}; 
 my $home_dir = $ENV{'HOME'}; 
 my $repo_path = $ENV{'HOME'} . "/example-cpx-vpx-for-kubernetes-2-tier-microservices/";
@@ -68,7 +64,7 @@ if ($CREATE_VPX_IMAGE eq "TRUE") {
     my $image_creation_pid = fork();
     if (not $image_creation_pid) {
        print ("\nImage Creation Child Process: Entering\n");
-       my $out = qx#gcloud -q compute images create netscaler12-1 --source-uri=$image_location --guest-os-features=MULTI_IP_SUBNET#;
+       my $out = qx#gcloud -q compute images create netscaler12-1 --source-uri=gs://gcp-vpximage/NSVPX-GCP-12.1-51.19_nc.tar.gz --guest-os-features=MULTI_IP_SUBNET#;
 
        print ("\nOutput of Image Creation is \n$out\n");
        print ("\nImage Creation Child Process: Exiting\n");
