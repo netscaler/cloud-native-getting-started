c5de95f3b43af7af6b2b51838c60baa9dcdfe69e
Avinash Kumar <vavinash.kumar@citrix.com>
2019-05-16 21:53:32 +0530

    helm package installation

--- gcp/citrixadm-config-files/helm/helm_install.sh
+++ gcp/citrixadm-config-files/helm/helm_install.sh
@@ -1,4 +1,4 @@
 wget https://storage.googleapis.com/kubernetes-helm/helm-v2.12.1-linux-amd64.tar.gz
 tar -xvf helm-v2.12.1-linux-amd64.tar.gz
 cd linux-amd64/
-sudo cp helm /usr/local/bin
+sudo cp helm /usr/local/sbin
