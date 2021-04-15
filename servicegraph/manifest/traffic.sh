02002b0be1c85993a67ea55546d4bebee5988480
Subash Dangol <subash.dangol@citrix.com>
2021-04-15 19:27:55 +0530

    Adding Servicegraph using Dual Tier Topology
    Signed-off-by: Subash Dangol <subash.dangol@citrix.com>

---
+++ servicegraph/manifest/traffic.sh
@@ -0,0 +1,19 @@
+export VIP=<VirtualIP>
+#!/bin/sh
+while true
+do
+curl http://$VIP/tv-shows -H "Host: netflix-frontend-service"
+sleep 3 
+curl http://$VIP/tv-shows -H "Host: netflix-frontend-service"
+sleep 3
+curl http://$VIP/movies -H "Host: netflix-frontend-service"
+sleep 3
+curl http://$VIP/recommendation-engine?type=trending -H "Host: netflix-frontend-service"
+sleep 3
+curl http://$VIP/recommendation-engine?type=similar-shows -H "Host: netflix-frontend-service"
+sleep 3
+curl http://$VIP/recommendation-engine?type=mutual-friends-interests -H "Host: netflix-frontend-service"
+sleep 3
+curl http://$VIP/recommendation-engine?type=best-shows -H "Host: netflix-frontend-service"
+sleep 3
+done
